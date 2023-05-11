88ed667602b9ddc8fa42312e77c6576b85c3935a
Auwal <auwallinus@gmail.com>
2023-05-11 06:02:20 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
+"""A recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit"""
+
 import requests
-"""
-function that queries reddit api
-"""
 
 
 def recurse(subreddit, hot_list=[]):
-    """
-    recursive function that queries the reddit api and returns a list
-    containing the titles of all hot articles for a given reddit.
-    if no results are found for the given subreddit the function
-    should return none
-    """
-    if type(subreddit) is list:
-        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
-        url = "{}&after={}".format(url, subreddit[1])
+    """returns the number of all hot articles of a given subreddit"""
+    r = requests.get(r'https://www.reddit.com/r/{}/hot/.json'
+                     .format(subreddit), headers={'User-agent': 'x'},
+                     allow_redirects=False)
+    l = r.json().get('data').get('children')
+    if r.status_code != 200:
+        return None
+    # return [subreddit] + hot_list
+    if hot_list == []:
+        return None
     else:
-        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
-        subreddit = [subreddit, ""]
-    header = {'User-Agent': 'CustomClient/1.0'}
-    req = requests.get(url, headers=header, allow_redirects=False)
-    if req.status_code != 200:
-        return (None)
-    req = req.json()
-    if "data" in req:
-        data = req.get("data")
-        if not data.get("children"):
-            return (hot_list)
-        for post in data.get("children"):
-            hot_list += [post.get("data").get("title")]
-        if not data.get("after"):
-            return (hot_list)
-        subreddit[1] = data.get("after")
-        recurse(subreddit, hot_list)
-        if hot_list[-1] is None:
-            del hot_list[-1]
-        return (hot_list)
-    else:
-        return (None)
+        k = hot_list[0]
+        small_list = hot_list[1:]
+        return k + recurse(small_list)
