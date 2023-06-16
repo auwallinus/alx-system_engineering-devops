1af0f2f2ce46505b0db2bfb0878dddd387ba1a63
Auwal <auwallinus@gmail.com>
2023-06-16 09:43:35 +0000

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,20 +6,20 @@ package {'nginx':
 
 exec {'install':
   command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-  provider => shell,
+  provider => ubuntu,
 
 }
 
 exec {'Hello':
   command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
+  provider => ubuntu,
 }
 
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.max.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
-  provider => shell,
+  provider => ubuntu,
 }
 
 exec {'run':
   command  => 'sudo service nginx restart',
-  provider => shell,
+  provider => ubuntu,
 }
